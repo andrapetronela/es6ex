0576567979e113a888dad8f311f547cac1860ac3
Andra <andra.popa1989@gmail.com>
2018-12-04 15:53:52 +0000

    add diff 2 arrays challenge

--- fcc.js
+++ fcc.js
@@ -424,7 +424,21 @@
 //}
 //console.log(nonMutatingSort(globalArray));
 
+// 18. Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
 
+//function diffArray(arr1, arr2) {
+//  let newArr = arr1.concat(arr2);
+//    
+//  let result = [];
+//  let countNum = newArr.filter(item => {
+//      if (!arr1.includes(item) || !arr2.includes(item)) {
+//          return result.push(item);
+//      }
+//  })
+//  return result;
+//}
+//
+//diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
 
 
